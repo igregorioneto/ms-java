aa4b6ac8bbaeb031332829bbdd4acbc58fe3858b
Gregório Neto <igregoriodoneto@outlook.com>
2022-01-31 10:22:35 -0300

    MS - Comunicação entre microservices

--- hr-payroll/src/main/java/com/greg/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/greg/hrpayroll/services/PaymentService.java
@@ -10,21 +10,29 @@ import org.springframework.web.client.RestTemplate;
 
 import com.greg.hrpayroll.entities.Payment;
 import com.greg.hrpayroll.entities.Worker;
+import com.greg.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
+//	@Value("${hr-worker.host}")
+//	private String workerHost;
 	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
+	
+//	private RestTemplate restTemplate;
+	
+//	public Payment getPayment(long workerId, int days) {
+//		Map<String, String> uriVariables = new HashMap<>();
+//		uriVariables.put("id", ""+workerId);
+//		
+//		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+//		return new Payment(worker.getName(), worker.getDailyIncome(), days);
+//	}
 	
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workerId);
-		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 
