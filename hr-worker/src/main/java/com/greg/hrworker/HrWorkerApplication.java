367db9600351173d43a536aa7ee84278f9d578f6
Gregório Neto <igregoriodoneto@outlook.com>
2022-02-01 20:47:18 -0300

    MS - Clientes Eureka

--- hr-worker/src/main/java/com/greg/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/greg/hrworker/HrWorkerApplication.java
@@ -2,7 +2,9 @@ package com.greg.hrworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrWorkerApplication {
 
