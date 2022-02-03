c79a76b4a1aa0c44a75f9ca68c8fe81a21e24f12
Gregório Neto <igregoriodoneto@outlook.com>
2022-02-03 16:21:18 -0300

    MS - Carga de dados User

--- hr-eureka-server/src/main/java/com/greg/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/greg/hreurekaserver/HrEurekaServerApplication.java
@@ -1,13 +1,16 @@
 package com.greg.hreurekaserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaServerApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrEurekaServerApplication.class, args);
 	}
